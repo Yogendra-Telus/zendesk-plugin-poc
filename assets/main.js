2d22c2a7503fb139cdaa653ea8d9aa311b96351e
Arpit Kumar <arpit.kumar01@telusinternational.com>
2023-02-21 18:30:11 +0530

    Updated UI and Added logic for send message to send box of chat

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,8 @@
 const languageKey = "LANGUAGES";
 
 var client = ZAFClient.init();
-client.invoke("resize", { width: "100%", height: "280px" });
+
+client.invoke("resize", { width: "100%", height: "600px" });
 
 const fetchLanguageList = () => {
   const options = {
@@ -16,68 +17,141 @@ const fetchLanguageList = () => {
 
 fetchLanguageList();
 
-const detectedLanguage = document.getElementById("detected-language");
-const detectedText = document.getElementById("detected-text");
-const translatedText = document.getElementById("translated-text");
-const copyBtn = document.getElementById("copy-btn");
+const chatRoom = document.getElementById("chat-container");
+const sendBtn = document.getElementById("send-btn");
+const sendText = document.getElementById("send-txt");
+let updateLangCode = "en";
 
 client.get(["ticket.conversation"]).then(function (data) {
   var conversionLength = data["ticket.conversation"].length;
 
   if (conversionLength) {
-    const conversionDetail = data["ticket.conversation"][conversionLength - 1];
-    const textToTranslate = conversionDetail.message.content;
+    const conversations = data["ticket.conversation"];
 
-    detectedText.innerText = textToTranslate;
-
-    if (textToTranslate) detectLanguageAndTranslate(textToTranslate, client);
+    detectLanguageAndTranslate(conversations, client);
   }
 });
 
-function detectLanguageAndTranslate(text, client) {
-  const url = "https://tl-qa.xavlab.xyz/extension/api/detect";
-  const payload = {
-    Text: text,
-  };
+function detectLanguageAndTranslate(conversations, client) {
+  const conversationElements = [];
 
-  const optionsForDetect = {
-    url,
+  conversations.forEach((element, elementId) => {
+    const text = element.message.content;
+    const payload = {
+      Text: text,
+    };
+
+    const optionsForDetect = {
+      url: "https://tl-qa.xavlab.xyz/extension/api/detect",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(payload),
+    };
+
+    client.request(optionsForDetect).then(async (response) => {
+      const { result } = response;
+      const languages = await JSON.parse(localStorage.getItem(languageKey));
+
+      if (!!result && Array.isArray(result)) {
+        const languageCode = result[0].language;
+
+        if (elementId === conversations.length - 1)
+          updateLangCode = languageCode;
+
+        const languageDetail = languages.find(
+          (lan) => lan.code === languageCode
+        );
+
+        // detectedLanguage.innerText = languageDetail?.name || "";
+
+        const optionsForTranslate = {
+          url: `https://tl-qa.xavlab.xyz/extension/api/translator?to=en&from=${languageCode}`,
+          type: "POST",
+          contentType: "application/json",
+          data: JSON.stringify(payload),
+        };
+
+        client.request(optionsForTranslate).then((response) => {
+          const { result } = response;
+
+          if (!!result && Array.isArray(result)) {
+            const translatedTextFromAPI = result[0].translations[0].text;
+
+            // translatedText.innerText = translatedTextFromAPI || "";
+
+            const chatContainer = document.createElement("div");
+            chatContainer.id = `${elementId}`;
+            chatContainer.classList.add("chat-item");
+
+            const headingContainer = document.createElement("div");
+            headingContainer.classList.add("heading");
+
+            const authorName = document.createElement("div");
+            authorName.innerText = element.author.name;
+
+            const timeOfMsg = document.createElement("div");
+            const time = new Date(element.timestamp).toLocaleTimeString();
+            const date = new Date(element.timestamp).toLocaleDateString();
+            timeOfMsg.innerText = date + "  " + time;
+
+            headingContainer.appendChild(authorName);
+            headingContainer.appendChild(timeOfMsg);
+
+            const messageContainer = document.createElement("div");
+            messageContainer.classList.add("msg-container");
+
+            const originalMsg = document.createElement("div");
+            originalMsg.innerText = text;
+            originalMsg.style.marginBottom = "5px";
+
+            const translatedMsg = document.createElement("div");
+            translatedMsg.innerText = `Translated Text:  ${translatedTextFromAPI}`;
+
+            messageContainer.appendChild(originalMsg);
+            messageContainer.appendChild(translatedMsg);
+
+            chatContainer.appendChild(headingContainer);
+            chatContainer.appendChild(messageContainer);
+
+            conversationElements.push({ id: elementId, node: chatContainer });
+          }
+        });
+      }
+    });
+  });
+
+  const intervalId = setInterval(() => {
+    if (conversations.length === conversationElements.length) {
+      conversationElements.sort((a, b) => a.id - b.id);
+
+      conversationElements.forEach((e) => {
+        chatRoom.appendChild(e.node);
+      });
+
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+sendBtn.addEventListener("click", () => {
+  const payload = {
+    Text: sendText.value,
+  };
+  const optionsForTranslate = {
+    url: `https://tl-qa.xavlab.xyz/extension/api/translator?to=${updateLangCode}&from=en`,
     type: "POST",
     contentType: "application/json",
     data: JSON.stringify(payload),
   };
 
-  client.request(optionsForDetect).then(async (response) => {
+  client.request(optionsForTranslate).then((response) => {
     const { result } = response;
-    const languages = await JSON.parse(localStorage.getItem(languageKey));
-
     if (!!result && Array.isArray(result)) {
-      const languageCode = result[0].language;
-
-      const languageDetail = languages.find((lan) => lan.code === languageCode);
-
-      detectedLanguage.innerText = languageDetail?.name || "";
-
-      const optionsForTranslate = {
-        url: `https://tl-qa.xavlab.xyz/extension/api/translator?to=en&from=${languageCode}`,
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(payload),
-      };
-
-      client.request(optionsForTranslate).then((response) => {
-        const { result } = response;
-
-        if (!!result && Array.isArray(result)) {
-          const translatedTextFromAPI = result[0].translations[0].text;
-
-          translatedText.innerText = translatedTextFromAPI || "";
-        }
-      });
+      client
+        .invoke("ticket.comment.appendText", result[0].translations[0].text)
+        .then(() => {
+          sendText.value = "";
+        });
     }
   });
-}
-
-copyBtn.addEventListener("click", () => {
-  navigator.clipboard.writeText(translatedText.innerText);
 });
